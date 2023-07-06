9dea29df7267656ee84c908f516899a02072594a
chocoscoding <chocoscoding@gmail.com>
2023-07-06 23:54:21 +0100

    .

--- app/components/home/Post.tsx
+++ app/components/home/Post.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Post = () => {
   return (
-    <div className="bg-blue-dark-70 h-40 w-full mb-4 rounded-md">
-      <div className="span">s</div>
+    <div className="bg-dark-30 h-56 w-full mb-4 rounded-xl flex p-4">
+      <Image
+        src={
+          "https://images.unsplash.com/photo-1635006459494-c9b9665a666e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+        }
+        width={200}
+        height={320}
+        alt="post-Image"
+        className="rounded-lg"
+      />
+      <div className=""></div>
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
         domains: [
             'res.cloudinary.com',
             'avatars.githubusercontent.com',
-            'lh3.googleusercontent.com'
+            'lh3.googleusercontent.com',
+            'images.unsplash.com'
         ]
 
     }
