063490da558ff15cc5e28dc656e6b29e4343e6f5
chocoscoding <chocoscoding@gmail.com>
2023-07-09 00:34:54 +0100

    meetup page

--- app/(main)/meetups/MeetupClient.tsx
+++ app/(main)/meetups/MeetupClient.tsx
@@ -8,8 +8,8 @@ import Image from "next/image";
 const MeetupClient = () => {
   return (
     <div>
-      <div className=" w-full flex flex-wrap gap-4">
-        <section className="sticky top-[60px] rounded-xl bg-dark-40 w-3/12 max-w-[300px] p-4 h-fit">
+      <div className="w-full flex flex-wrap gap-4">
+        <section className="sticky top-[60px] rounded-xl bg-dark-30 w-3/12 max-w-[300px] p-4 h-fit shrink-0">
           <div className="flex justify-between items-center">
             <p className="text-xl font-medium">Categories</p>
             <Arrow1 className=" rotate-90 scale-75" />
@@ -49,8 +49,11 @@ const MeetupClient = () => {
             </div>
           </main>
         </section>
-        <Meetups />
-        <section className="sticky top-[60px]  rounded-xl bg-[#FF7C4D] w-4/12 max-w-[310px] p-4 h-[180px]">
+        {/* <section className=" flex flex-col gap-2">
+          <Meetups />
+        </section> */}
+        <div className="w-full border"></div>
+        <section className="sticky top-[60px] rounded-xl bg-[#FF7C4D] w-4/12 max-w-[310px] p-4 h-[180px] shrink-0">
           <p className="font-semibold text-lg">Host a Meetup</p>
           <p className="text-xs my-4">Find other Hipnoders in your area so you can learn, share, and work together.</p>
 
--- app/(main)/meetups/Meetups.tsx
+++ app/(main)/meetups/Meetups.tsx
@@ -1,25 +1,57 @@
+import Tags from "@/app/components/Tags";
+import Image from "next/image";
 import React, { Suspense } from "react";
 interface MeetupTypes {}
 const Meetups: React.FC<MeetupTypes> = ({}) => {
   console.log();
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex-1 flex flex-col gap-2">
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
-        <div className="w-full bg-slate-500 h-20"></div>
+      <div className="w-full bg-dark-30 min-h-[200px] rounded-xl flex flex-col p-2.5">
+        <div className="flex gap-2">
+          <div className="rounded-md overflow-hidden w-[85px] h-[85px] shrink-0">
+            <Image
+              src={
+                "https://images.unsplash.com/photo-1635006459494-c9b9665a666e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+              }
+              width={500}
+              height={500}
+              alt="post-Image"
+              className="rounded-lg"
+            />
+          </div>
+
+          <section className="flex justify-between flex-1">
+            <div className="flex flex-col gap-2">
+              <p className="overflow-clip truncate">UIHUT - Crunchbase Company Profile & Funding</p>
+              <p className=" text-xs text-secondary-30">
+                UIHUT <span></span> <span>Sylhet, Bangladesh</span>
+              </p>
+            </div>
+            <div className="rounded-md flex flex-col items-center h-[4.3rem] lg1:h-[3.5rem] px-2 shadow-md dark:shadow-dark-10">
+              <p className="">FEB</p>
+              <p className="text-blue-80 font-extrabold text-2xl lg1:text-xl">7</p>
+            </div>
+          </section>
+        </div>
+        <p className="w-full h-11 mb-2 text-xs">
+          Dribbble Meetups are a chance for designers to socialize, talk shop, and foster their local design communities. They are
+          self-organized events that range from the casual, five-person, coffee-shop roundtable to the chef-catered, bar-tended, 200-person
+          extravaganza...
+        </p>
+        <div className="w-full flex gap-3 lg1:gap-1.5 mt-5 lg1:mt-4 lg1:mb-1 overflow-x-auto scrollbar-none flex-wrap">
+          <Tags
+            label="Enterprenureship"
+            classname="text-sm sm1:text-xss text-secondary-50 py-1.5 sm1:p-1 hover:underline cursor-pointer"
+          />
+          <Tags
+            label="International"
+            classname="text-sm sm1:text-xss text-secondary-50 py-1.5 hover:underline cursor-pointer"
+          />
+          <Tags
+            label="Sustainability"
+            classname="text-sm sm1:text-xss text-secondary-50 py-1.5 hover:underline cursor-pointer"
+          />
+        </div>
       </div>
     </Suspense>
   );
