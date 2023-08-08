d77c20239398cd0d7a5b184f4ef600f110873052
chocoscoding <chocoscoding@gmail.com>
2023-08-08 20:26:00 +0100

    profile page init

---
+++ app/(main)/profile/page.tsx
@@ -0,0 +1,14 @@
+import Checkbox from "@/app/components/Checkbox";
+import { Arrow1, ArrowDown } from "@/app/components/Icons";
+import React from "react";
+import MeetupClient from "./MeetupClient";
+
+const Page = () => {
+  return (
+    <main className="appScreen p-6 xl1:p-2 max-w-[1600px] m-auto">
+      <MeetupClient />
+    </main>
+  );
+};
+
+export default Page;
