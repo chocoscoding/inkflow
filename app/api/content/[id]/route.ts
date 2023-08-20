e94c8a821bced68063345b44b848cc34c797f8ae
chocoscoding <chocoscoding@gmail.com>
2023-08-20 21:17:21 +0100

    .

--- app/api/content/[id]/route.ts
+++ app/api/content/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export default async function DELETE(params:any){
+export default async function GET(params:any){
     console.log(params);
     
     return  NextResponse.json("Hello, Next.js!");
